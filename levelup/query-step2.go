2447dbf67f8061a01d486573686ab5d2fa61d47e
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-09-24 22:43:22 +0900

    feat: 問題追加

---
+++ levelup/query-step2.go
@@ -0,0 +1,31 @@
+package try
+
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	var N int
+// 	var K int
+
+// 	fmt.Scan(&N, &K)
+
+// 	exists := false
+	
+// 	for i := 0; i <= N - 1; i++ {
+// 		var num int
+// 		fmt.Scan(&num)
+// 		if K == num {
+// 			exists = true
+// 			break
+// 		} 
+// 	}
+
+// 	if exists {
+// 		fmt.Println("YES")
+// 	} else {
+// 		fmt.Println("NO")
+// 	}
+
+// }
