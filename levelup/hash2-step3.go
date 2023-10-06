59471c02d0ff82f239d0d3b4558e920f4f0ec37e
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-10-06 19:57:45 +0900

    feat: hash問題追加

--- levelup/hash2-step3.go
+++ levelup/hash2-step3.go
@@ -3,50 +3,51 @@ package levelup
 // package main
 
 // import (
-// 	"fmt"
+//     "fmt"
 // )
 
 // func main() {
-// 	var a, b, q int
-// 	fmt.Scan(&a, &b)
-// 	fmt.Scan(&q)
+//     table := make([][]int, 100)
+//     for i := 0; i < 100; i++ {
+//         table[i] = []int{}
+//     }
 
-// 	table := make([][]int, 100)
-// 	for i := 0; i < 100; i++ {
-// 		table[i] = []int{}
-// 	}
+//     var a, b, q int
+//     fmt.Scan(&a, &b)
+//     fmt.Scan(&q)
 
-// 	for i := 0; i < q; i++ {
-// 		var op, x int
-// 		fmt.Scan(&op, &x)
+//     for i := 0; i < q; i++ {
+//         var op, x int
+//         fmt.Scan(&op, &x)
 
-// 		hash := (a*x + b) % 100
+//         hash := (a*x + b) % 100
 
-// 		if op == 1 {
-// 			table[hash] = append(table[hash], x)
-// 		} else if op == 2 {
-// 			found := false
-// 			for _, value := range table[hash] {
-// 				if value == x {
-// 					found = true
-// 					break
-// 				}
-// 			}
-// 			if found {
-// 				fmt.Println("Yes")
-// 			} else {
-// 				fmt.Println("No")
-// 			}
-// 		}
-// 	}
+//         switch op {
+//         case 1:
+//             table[hash] = append(table[hash], x)
+//         case 2:
+//             found := false
+//             for _, value := range table[hash] {
+//                 if value == x {
+//                     found = true
+//                     break
+//                 }
+//             }
+//             if found {
+//                 fmt.Println("Yes")
+//             } else {
+//                 fmt.Println("No")
+//             }
+//         }
+//     }
 
-// 	for i := 0; i < 100; i++ {
-// 		for j, value := range table[i] {
-// 			if j > 0 {
-// 				fmt.Print(" ")
-// 			}
-// 			fmt.Print(value)
-// 		}
-// 		fmt.Println()
-// 	}
+//     for i := 0; i < 100; i++ {
+//         for j := 0; j < len(table[i]); j++ {
+//             if j > 0 {
+//                 fmt.Print(" ")
+//             }
+//             fmt.Print(table[i][j])
+//         }
+//         fmt.Println()
+//     }
 // }
